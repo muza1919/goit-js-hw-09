429061731916e1213ad14f58d8e49b1598fe458f
Alona <alena2003305@gmail.com>
2023-01-04 12:12:44 +0200

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let interval = null;
+// const interval = setInterval(() => {body.style.backgroundColor = getRandomHexColor}, 1000);
 
+startBtn.addEventListener('click', () => {
+    onClickStart();
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+});
+
+stopBtn.addEventListener('click', () => {
+    onClickStop();
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+});
+
+function onClickStart (){
+    interval = setInterval(() => {body.style.backgroundColor = getRandomHexColor()}, 1000);
+}
+
+function onClickStop(){
+    clearInterval(interval);
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
