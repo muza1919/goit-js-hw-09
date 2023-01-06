f65c61b318f32d72e748a6c8453d8cad18ee4286
Alona <alena2003305@gmail.com>
2023-01-06 22:33:55 +0200

    task 2 done

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,49 +3,52 @@ import flatpickr from "flatpickr";
 // Додатковий імпорт стилів
 import "flatpickr/dist/flatpickr.min.css";
 const input = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('button[data-start]');
+const dateDays = document.querySelector('span[data-days]');
+const dateHours = document.querySelector('span[data-hours]');
+const dateMinutes = document.querySelector('span[data-minutes]');
+const dateSeconds = document.querySelector('span[data-seconds]');
+
+startBtn.disabled = true;
 const options = {
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-      console.log(selectedDates[0]);
+        if (selectedDates[0] < new Date()){
+          window.alert("Please choose a date in the future");
+          startBtn.disabled = true;
+        }
+          startBtn.disabled = false;
     },
   };
 const fp = flatpickr(input, options);
 
-const startBtn = document.querySelector('button[data-start]');
-const dateDays = document.querySelector('button[data-days]');
-const dateHours = document.querySelector('button[data-hours]');
-const dateMinutes = document.querySelector('button[data-minutes]');
-const dateSeconds = document.querySelector('button[data-seconds]');
-// input.textContent
-const currentTime = Date.now();
-const endTime = options.onClose();
-console.log(endTime)
-
-// onChooseDate();
 
 startBtn.addEventListener('click', () => {
-    // const ms = endTime - currentTime;
-    // console.log(ms);
-    // const {daysMs, hoursMs, minutesMs, secondsMs} = convertMs(ms);
-
-    // dateDays.textContent = addLeadingZero(daysMs);
-    // dateHours.textContent = addLeadingZero(hoursMs);
-    // dateMinutes.textContent = addLeadingZero(minutesMs);
-    // dateSeconds.textContent = addLeadingZero(secondsMs);
+    const interval = setInterval(() => {
+      const currentTime = new Date();
+      const endTime = new Date(input.value);
+      // console.log(endTime);
+      const ms = endTime - currentTime;
+      // console.log(ms);
+      startBtn.disabled = true;
+      
+      if(ms >= 0){
+        const time = convertMs(ms);
+      // console.log(time.days);
+      dateDays.textContent = addLeadingZero(time.days);
+      dateHours.textContent = addLeadingZero(time.hours);
+      dateMinutes.textContent = addLeadingZero(time.minutes);
+      dateSeconds.textContent = addLeadingZero(time.seconds);
+      } else {
+        clearInterval(interval);
+      }
+    }, 1000);
 });
 
 
-
-// function onChooseDate(){
-//     if (endTime > currentTime){
-//         window.alert("Please choose a date in the future");
-//         startBtn.disabled = true;
-//     }
-//     startBtn.disabled = false;
-// }
 
 function addLeadingZero(value){
    return value.toString().padStart(2, '0');
@@ -71,6 +74,6 @@ function convertMs(ms) {
     return { days, hours, minutes, seconds };
   }
   
-  console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-  console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-  console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+  // console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+  // console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+  // console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
