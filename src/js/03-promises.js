abb34851b10a13ec26db14494b9543cde29a6de4
Alona <alena2003305@gmail.com>
2023-01-06 23:00:23 +0200

    task 3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
+const submit = document.querySelector('button[type="submit"]');
+const delay = document.querySelector('input[name="delay"]');
+const step = document.querySelector('input[name="step"]');
+const amount = document.querySelector('input[name="amount"]');
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+submit.addEventListener('click', e => {
+  e.preventDefault();
+  const firstDelay = Number(delay.value);
+  const delayStep = Number(step.value);
+  for (let i = 0; i < amount.value; i++) {
+    createPromise(i + 1, firstDelay + i * delayStep)
+      .then(({ position, delay }) => {
+        console.log(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        console.log(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+});
+
